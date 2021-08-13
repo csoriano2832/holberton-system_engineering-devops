4a1c4ecaf9b47d61e30d83876ea6da8e58ac7374
Christopher Soriano <77821793+csoriano2832@users.noreply.github.com>
2021-08-13 18:49:07 -0400

    delete repo with typo

--- 0x0A-configuration_mangement/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# This manifest creates a file in /tmp with specific requirements
-file { 'holberton':
-  path    => '/tmp/holberton',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  ensure  => 'present',
-  content => 'I love Puppet',
-}
